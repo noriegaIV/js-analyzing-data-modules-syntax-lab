ac0a44ee7a5b25c4fef9b4cf6407c2931081e5e4
Tony <tonytoroitich27@gmail.com>
2026-07-28 23:36:13 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+require("datejs");
 
+function combineUsers(...args) {
 
+  let combinedObject = {
+    users: []
+  };
+
+  for (let i = 0; i < args.length; i++) {
+    combinedObject.users = [...combinedObject.users, ...args[i]];
+  }
+
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+  return combinedObject;
+}
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  ...(typeof combineUsers !== "undefined" && { combineUsers })
 };
